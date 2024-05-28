50b8c59ed2edb43eb15c69327895195f24e0c5b0
peregomon <160238970+peregomon@users.noreply.github.com>
2024-05-28 11:54:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,22 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        exit(1);
+    }
+    char a;
+    std::string word;
+    BST<std::string> tree;
+    while (file.get(a)) {
+        if (isalpha(a)) {
+            word += tolower(a);
+        } else if (!word.empty()) {
+            tree.add(word);
+            word.clear();
+        }
+    }
+    file.close();
+    return tree;
 }
